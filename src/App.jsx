105651fe489eb562ed5b30e3df3b1237ed2017f2
deepansh <99.deepanshagrawal@gmail.com>
2023-06-05 21:56:29 +0530

    * Initializing repo.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="appRoot">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dignissimos accusamus beatae rem a temporibus quibusdam recusandae harum veniam accusantium ducimus, distinctio facere. Ea labore eligendi sed voluptates tempore delectus velit, necessitatibus laudantium! Dolorem fuga laboriosam beatae sint recusandae deleniti pariatur dignissimos, iure amet placeat libero voluptate excepturi magnam consequatur?
+    </div>
+  )
+}
+
+export default App
